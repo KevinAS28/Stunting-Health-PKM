b23812774c3233f1f1d50d6b23da950d15f5d914
KevinAS28 <kevinagusto28@gmail.com>
2022-06-10 07:41:12 +0700

    change max upload settings to 25 mb

--- stunting_backend/settings.py
+++ stunting_backend/settings.py
@@ -132,3 +132,5 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATA_UPLOAD_MAX_MEMORY = 26214400
