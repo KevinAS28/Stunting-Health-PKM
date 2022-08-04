7abe98efc2866c8628606f40321241d7edec696e
KevinAS28 <kevinagusto28@gmail.com>
2022-08-04 21:41:34 +0700

    updated deploy script

--- deploy.py
+++ deploy.py
@@ -4,6 +4,7 @@ from datetime import datetime as dt
 def log(msg, end='\n'):
     with open('deploypy_log.txt', 'a+') as fd:
         fd.write(f'{dt.now()}\n{msg}{end}\n{"*"*15}')
+    print(msg+end)
 
 os_commands = [
     'python3 manage.py makemigrations',
@@ -15,11 +16,17 @@ def unzip_static():
     if os.path.isfile('static.zip'):
         os.system('unzip static.zip')
 
-for command in os_commands:
-    try:
-        os.system(command)
-    except Exception as e:
-        log(f'ERROR command: {command}: {str(e)}')
-        exit(1)
+
+if __name__=='__main__':
+    log('unzipping static...')
+    unzip_static()
+    
+    for command in os_commands:
+        try:
+            log(f'running command: {command}')
+            os.system(command)
+        except Exception as e:
+            log(f'ERROR command: {command}: {str(e)}')
+            exit(1)
 
 exit(0)
