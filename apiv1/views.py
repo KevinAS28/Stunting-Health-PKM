12a44dd34487597d273734dfa710a8cde775b6dd
KevinAS28 <kevinagusto28@gmail.com>
2022-08-31 09:35:18 +0700

    devleop data stats

--- apiv1/views.py
+++ apiv1/views.py
@@ -663,10 +663,13 @@ def data_stats(request: WSGIRequest):
             continue
         availble_trace += 1
         childs_trace[child] = model_to_dict(the_traces[0])
+        trace_updated_date:datetime.date = the_traces[0].updated_at.date()
+        childs_trace[child]['updated_at'] = {'year': trace_updated_date.year, 'month': trace_updated_date.month, 'day': trace_updated_date.day}
     
-    #stunting bar and percentage
+    # stunting bar, percentage, trends
     growth_agemonth_stunting = dict()
     growth_group_count = dict()
+    growth_date_trends = dict()
 
     for child_id, trace in childs_trace.items():
         if trace is None:
@@ -675,20 +678,30 @@ def data_stats(request: WSGIRequest):
         age_in_month = trace['week']%4
         growth_level = trace['growth_level']
 
+        # Percentage
         if not (growth_level in growth_group_count):
             growth_group_count[growth_level] = 1
         else:
             growth_group_count[growth_level] += 1
-            
+        
+        # stunting bar
         if not (growth_level in growth_agemonth_stunting):
             growth_agemonth_stunting[growth_level] = dict()
-
         if not age_in_month in growth_agemonth_stunting[growth_level]:
             growth_agemonth_stunting[growth_level][age_in_month] = 1
         else:
             growth_agemonth_stunting[growth_level][age_in_month] += 1
-    
-    #
-    return JsonResponse({'child_traces': childs_trace, 'growth_agemonth_stunting': growth_agemonth_stunting, 'availble_trace': availble_trace, 'growth_group_count': growth_group_count, 'growth_group_total': sum(growth_group_count.values())})
+
+        # trends
+        trace_updated_at = datetime.date(**trace['updated_at'])
+        if not (trace_updated_at in growth_date_trends):
+            growth_date_trends[trace_updated_at] = 1
+        else:
+            growth_date_trends[trace_updated_at] += 1
+        
+    growth_date_trends = {f'{key.year}-{key.month}-{key.day}':growth_date_trends[key] for key in sorted(growth_date_trends.keys())}
+
+
+    return JsonResponse({'child_traces': childs_trace, 'growth_agemonth_stunting': growth_agemonth_stunting, 'availble_trace': availble_trace, 'growth_group_count': growth_group_count, 'growth_group_total': sum(growth_group_count.values()), 'growth_date_trends': growth_date_trends})
 
     
