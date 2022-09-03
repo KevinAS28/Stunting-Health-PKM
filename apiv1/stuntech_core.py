7b907899b10ffd7f9958a74a7c68bb5b7f733b4e
KevinAS28 <kevinagusto28@gmail.com>
2022-09-03 10:25:45 +0700

    try separate boy and girl z_score algorithm

--- apiv1/stuntech_core.py
+++ apiv1/stuntech_core.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import math
 from django.conf import settings
 
 tb = pd.read_csv(os.path.join(settings.BASE_DIR, 'apiv1', 'stunt_boy.csv'))
@@ -11,25 +12,29 @@ tg = pd.read_csv(os.path.join(settings.BASE_DIR, 'apiv1', 'stunt_girl.csv'))
 def stunting_classification(sex, age_month, height):
     if sex==0:
         table = tb
-        print('trace: boy table')
+        # print('trace: boy table')
     else:
         table = tg
-        print('trace: girl table')
+        # print('trace: girl table')
 
     data = table.loc[table['age_month']==age_month].iloc[0]
 
     median = float(data['Median'])
-    print('trace: median: ', median)
+    # print('trace: median: ', median)
     if height < median:
         sd = float(data['-1_SD'])
     elif height > median:
         sd = float(data['3_SD'])
     else:
         return 0, 0
-    print('trace sd: ', sd)
-    z_score = ((height-median)/abs(median-sd))
 
-    print('trace:', z_score)
+    # print('trace sd: ', sd)
+
+    z_score = (((height-median)/abs(median-sd)))
+    # print('trace z_score ori:', z_score)
+    # z_score = float(str(z_score)[:3]) if z_score < 0 else float(str(z_score)[:2])
+    # print('trace z_score ROUNDED:', z_score)
+    
     if sex==0:
         if z_score < -3:
             return -2, z_score
@@ -40,13 +45,43 @@ def stunting_classification(sex, age_month, height):
         else:
             return 2, z_score
     else:
-        if z_score <= -3.1:
+        if z_score <= -3.1: # Beda tipis
             return -2, z_score
         elif (-3.1 < z_score < -2):
             return -1, z_score      
         elif -2 <= z_score <= 2:
             return 1, z_score
         else:
-            return 2, z_score        
+            return 2, z_score       
+
+    # if z_score < -3:
+    #     return -2, z_score
+    # elif -3 <= z_score < -2:
+    #     return -1, z_score      
+    # elif -2 <= z_score <= 2:
+    #     return 1, z_score
+    # else:
+    #     return 2, z_score
+
 if __name__=='__main__':
-    print(stunting_classification(1, 0, 43.6))
+    #1: cewe, 0: cowo.
+    test_table = [
+        ([1, 0, 53], 2),
+        ([1, 0, 43.6], -1),
+        ([1, 0, 43.7], -1),
+        ([1, 0, 43.5], -2),
+        ([0, 0, 46.1], 1),
+
+        ([0, 52, 110], 1),
+    ]
+    salah = False
+    for test in test_table:
+        status, z_score = stunting_classification(*test[0])
+        if status!=test[1]:
+            print(f'SALAH: {test}, {status}')
+            salah = True
+    if not salah:
+        print('TESTING SMUA BERHASIL')
+
+    
+    # print(stunting_classification(1, 0, 53))
