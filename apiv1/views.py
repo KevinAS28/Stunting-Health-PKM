fdb21e112eae519d24fae3c353aef416e4c29db7
KevinAS28 <kevinagusto28@gmail.com>
2022-09-03 20:22:42 +0700

    change test stunting core to web production

--- apiv1/views.py
+++ apiv1/views.py
@@ -686,7 +686,7 @@ def data_stats(request: WSGIRequest):
         
         # stunting bar
         if not (growth_level in growth_agemonth_stunting):
-            growth_agemonth_stunting[growth_level] = dict()
+            growth_agemonth_stunting[int(growth_level)] = dict()
         if not age_in_month in growth_agemonth_stunting[growth_level]:
             growth_agemonth_stunting[growth_level][age_in_month] = 1
         else:
@@ -703,7 +703,7 @@ def data_stats(request: WSGIRequest):
             growth_date_trends[growth_level][trace_updated_at] += 1
         
     growth_date_trends = {key1:{f'{key.year}-{key.month}-{key.day}':growth_date_trends[key1][key] for key in sorted(growth_date_trends[key1].keys())} for key1 in growth_date_trends}
-    growth_agemonth_stunting = {key1:{f'{key.year}-{key.month}-{key.day}':growth_agemonth_stunting[key1][key] for key in sorted(growth_agemonth_stunting[key1].keys())} for key1 in growth_agemonth_stunting}
+    growth_agemonth_stunting = {key1:{key:growth_agemonth_stunting[key1][key] for key in sorted(growth_agemonth_stunting[key1].keys())} for key1 in growth_agemonth_stunting}
     
 
     return JsonResponse({'child_traces': childs_trace, 'growth_agemonth_stunting': growth_agemonth_stunting, 'availble_trace': availble_trace, 'growth_group_count': growth_group_count, 'growth_group_total': sum(growth_group_count.values()), 'growth_date_trends': growth_date_trends})
